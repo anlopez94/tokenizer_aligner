c9016385cdca5a5ec40a2230009f52dbb51e92b5
anlopez94 <angelalopezcardona94@gmail.com>
2024-06-21 15:20:34 +0200

    .

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 setup(
-    name="tokenizer_aligner",
+    name="tokenizeraligner",
     version="0.1",
     packages=find_packages(),
     install_requires=requirements,
